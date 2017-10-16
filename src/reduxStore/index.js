17f855eb002ed2ea7bd6db04c413c5fdb6525788
Rosas <rosas@Davids-MBP.lan>
2017-10-16 23:50:08 +0200

    Added whiteboards to redux store

--- src/reduxStore/index.js
+++ src/reduxStore/index.js
@@ -2,13 +2,14 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './config/';
 import { loadNotes } from './config/notes';
+import { loadWhiteboards } from './config/whiteboards';
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable */
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
-
+store.dispatch(loadWhiteboards());
 store.dispatch(loadNotes());
 
 export default store;
