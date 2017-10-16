6ebbe1bd1dc9e225610351e07389c59882802eaa
Rosas <rosas@Davids-MBP.lan>
2017-10-16 16:42:58 +0200

    CSS for whiteboard list

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,12 +4,24 @@ import './Home.css';
 
 const Home = () => (
   <div className="HomeContainer">
-    <header>
-      <div className="logo" /><h1>Flogg<strong>IT</strong></h1>
-    </header>
-    <div className="HomeContainer-whiteboard-list">
-      <NavLink to="whiteboard" activeClassName="Home-active-link">Whiteboard</NavLink>
-      <NavLink to="/" activeClassName="Home-active-link">Home</NavLink>
+    <NavLink to="/" activeClassName="Home-active-link">
+      <header>
+        <div className="logo" /><h1>Flogg<strong>IT</strong></h1>
+        <button
+          type="button"
+          className="HomeContainer-create-button"
+        >
+          Create whiteboard
+        </button>
+      </header>
+    </NavLink>
+    <div className="HomeContainer-whiteboard-list Note">
+      <h1>Whiteboards</h1>
+      <ul>
+        <li>
+          <NavLink to="whiteboards" activeClassName="Home-active-link">Whiteboard</NavLink>
+        </li>
+      </ul>
     </div>
   </div>
 );
